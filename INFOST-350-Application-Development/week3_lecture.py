4685fe23b6c43af9086a89e646ce0234fe86fe90
Boe6Eod7Nty <benjamer02@gmail.com>
2024-09-18 15:30:23 -0500

    end-of-class commit

--- INFOST-350-Application-Development/week3_lecture.py
+++ INFOST-350-Application-Development/week3_lecture.py
@@ -1,13 +1,18 @@
 # week 3 lecture / lab code stuffs
 
+# Hello World print
 print("Hello World! \n")
 
+# set variables in memory
 name = "Ben Anderson"
 major = "Information Science and Technology"
 hobbies = "Software development, video games, golf, television/movies"
 favoriteMedia = "Futurama"
+careerQuestion = "How can I best get a green github profile?"
 
-print("Name:", name)
-print("Major:", major)
-print("Hobbies:", hobbies)
-print("Favorite Media:", favoriteMedia)
+# print variables with string context
+print("\tMy Name:", name)
+print("\tMajor:", major)
+print("\tHobbies:", hobbies)
+print("\tFavorite Media:", favoriteMedia)
+print("\tCareer Question:", careerQuestion,"\n")
