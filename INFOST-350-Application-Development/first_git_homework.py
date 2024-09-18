521dc0a665abd54163ac4252bdaef6cb974b2aa1
Boe6Eod7Nty <benjamer02@gmail.com>
2024-09-18 15:10:08 -0500

    week3 lecture/lab started

--- INFOST-350-Application-Development/first_git_homework.py
+++ INFOST-350-Application-Development/first_git_homework.py
@@ -4,7 +4,3 @@ A = 4
 if A == 4:
     print("success")
 
-
-if A == 4:
-    print("success")
-
---
+++ INFOST-350-Application-Development/week3_lecture.py
@@ -0,0 +1,13 @@
+# week 3 lecture / lab code stuffs
+
+print("Hello World! \n")
+
+name = "Ben Anderson"
+major = "Information Science and Technology"
+hobbies = "Software development, video games, golf, television/movies"
+favoriteMedia = "Futurama"
+
+print("Name:", name)
+print("Major:", major)
+print("Hobbies:", hobbies)
+print("Favorite Media:", favoriteMedia)
