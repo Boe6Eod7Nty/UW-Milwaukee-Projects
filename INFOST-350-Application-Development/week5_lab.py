968ba6d7a27ff180f3e137f8dd200c70ad68ddc6
Boe6Eod7Nty <benjamer02@gmail.com>
2024-10-08 16:55:41 -0500

    Completed week 5 madlibs lab work

--- INFOST-350-Application-Development/week5_lab.py
+++ INFOST-350-Application-Development/week5_lab.py
@@ -7,17 +7,40 @@
 # First names of 5 favorite friends, family, or special others
 # 5 favorite expressions or greetings when seeing someone
 
-# Create a print statement that performs the fokllowing story telling where 1 is index 1.
+# Create a print statement that performs the following story telling where 1 is index 1.
 # Repeat for index 0-5 when you complete for index 1.
 
 # In ListA(1) and ListB(1) I love to ear ListC(1) while speaking ListD(1) and enjoing ListE(1)
 #       at ListF(1) with ListG(1) while saying ListH(1) to people walking by
 
-ListA = []
-ListB = []
-ListC = []
-ListD = []
-ListE = []
-ListF = []
-ListG = []
-ListH = []
+ListA = ["New York City","Tokyo","London","Los Angeles","Hong Kong"]
+ListB = ["Milwaukee","New York City","Los Angeles","Grand Rapids","Las Vegas"]
+ListC = ["Enchaladas","Eggs and waffles","Shwarma","Burgers","Chicago Deep Dish Pizza"]
+ListD = ["English","Japanese","Swedish","Korean","Esperanto"]
+ListE = ["December, 1963 (Oh, What a Night!)","Margaritaville","The Rubberband Man","Brandy (You\'re a Fine Girl)","Once in a Lifetime"]
+ListF = ["La Masa","Shaker\'s","Ian\'s Pizza","AJ Bombers","Taco Bell"]
+ListG = ["Liam","Caleb","Austin","Garrett","John"]
+ListH = ["\'What's up?\'","\'Hey!\'","\'How\'ve you been?\'","\'Howdy\'","\'Hello\'"]
+
+
+for i in range(0,5):
+    sentance = (
+                "In "
+                + ListA[i] # world city
+                + " and "
+                + ListB[i] # US city
+                + " I love to eat "
+                + ListC[i] # food
+                + " while speaking "
+                + ListD[i] # language
+                + " and enjoying "
+                + ListE[i] # song
+                + " at "
+                + ListF[i] # resturant
+                + " with "
+                + ListG[i] # friend
+                + " while saying "
+                + ListH[i] # greeting
+                + " to people walking by.\n"
+                )
+    print(sentance)
